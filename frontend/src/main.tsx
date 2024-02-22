27971b2559387ed3f81378490b946fa246babe3e
brandon ly <brandon@brndn.ly>
2024-02-22 18:40:23 -0500

    feat(frontend): setup authenticated routes

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,12 +2,20 @@ import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
 import "./index.css";
+import { AuthProvider, useAuth } from "./auth";
 
 // Import the generated route tree
 import { routeTree } from "./routeTree.gen";
 
 // Create a new router instance
-const router = createRouter({ routeTree });
+const router = createRouter({
+	routeTree,
+	defaultPreload: "intent",
+	context: {
+		// biome-ignore lint/style/noNonNullAssertion: <explanation>
+		auth: undefined!,
+	},
+});
 
 // Register the router instance for type safety
 declare module "@tanstack/react-router" {
@@ -16,14 +24,23 @@ declare module "@tanstack/react-router" {
 	}
 }
 
+function InnerApp() {
+	const auth = useAuth();
+	return <RouterProvider router={router} context={{ auth }} />;
+}
+
+function App() {
+	return (
+		<AuthProvider>
+			<InnerApp />
+		</AuthProvider>
+	);
+}
+
 // Render the app
 // biome-ignore lint/style/noNonNullAssertion: <explanation>
 const rootElement = document.getElementById("app")!;
 if (!rootElement.innerHTML) {
 	const root = ReactDOM.createRoot(rootElement);
-	root.render(
-		<StrictMode>
-			<RouterProvider router={router} />
-		</StrictMode>,
-	);
+	root.render(<App />);
 }
--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -1,20 +1,59 @@
-import { createRootRoute, Link, Outlet } from "@tanstack/react-router";
+import * as React from "react";
+import {
+	Link,
+	Outlet,
+	createRootRouteWithContext,
+} from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 
-export const Route = createRootRoute({
-	component: () => (
+import { useAuth, type AuthContext } from "../auth";
+
+interface MyRouterContext {
+	auth: AuthContext;
+}
+
+export const Route = createRootRouteWithContext<MyRouterContext>()({
+	component: RootComponent,
+});
+
+function RootComponent() {
+	const auth = useAuth();
+	return (
 		<>
-			<div className="p-2 flex gap-2">
-				<Link to="/" className="[&.active]:font-bold">
+			<div className="p-2 flex gap-2 text-lg">
+				<Link
+					to="/"
+					activeProps={{
+						className: "font-bold",
+					}}
+					activeOptions={{ exact: true }}
+				>
 					Home
 				</Link>{" "}
-				<Link to="/about" className="[&.active]:font-bold">
-					About
-				</Link>
+				{auth.isAuthenticated ? (
+					<Link
+						to={"/dashboard"}
+						activeProps={{
+							className: "font-bold",
+						}}
+					>
+						Dashboard
+					</Link>
+				) : (
+					<Link
+						to={"/login"}
+						activeProps={{
+							className: "font-bold",
+						}}
+						search={{ redirect: "/" }}
+					>
+						Login
+					</Link>
+				)}
 			</div>
 			<hr />
 			<Outlet />
-			<TanStackRouterDevtools />
+			<TanStackRouterDevtools position="bottom-right" initialIsOpen={false} />
 		</>
-	),
-});
+	);
+}
