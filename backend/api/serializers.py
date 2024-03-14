5d5f1bd935bf7b8a4241b50f2dea169079c5f09d
brandon ly <brandon@brndn.ly>
2024-03-14 15:39:29 -0400

    feat(backend): register endpoint

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,7 +1,20 @@
 from rest_framework import serializers
 from api.models import Query
+from django.contrib.auth.models import User
 
 class QuerySerializer(serializers.ModelSerializer):
     class Meta:
         model = Query
         fields = '__all__'
+
+
+class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    def create (self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
+    
+    class Meta:
+        model = User
+        fields = ('username', 'password')
