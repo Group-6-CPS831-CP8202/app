894416a5e651a2f72283453ed69a156840b3e71f
Brandon Ly <git@brndn.ly>
2024-03-25 16:37:00 -0400

    test(backend):

--- backend/api/tests.py
+++ backend/api/tests.py
@@ -10,3 +10,15 @@ class QueryDetailTests(APITestCase):
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.json()['result']['resource_id'], 'fac950c0-00d5-4ec1-a4d3-9cbebf98a305')
+
+class RegisterEndPointTests(APITestCase):
+    def test_register_endpoint(self):
+        url = '/api/register'
+        data = {
+            'username': 'test',
+            'password': 'test',
+        }
+        response = self.client.post(url, data, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.data, {'message': 'success'})
