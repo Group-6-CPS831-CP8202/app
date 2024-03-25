00337d13b7ee3aab69fb73052b04b31d6adb73a8
Brandon Ly <git@brndn.ly>
2024-03-25 15:51:34 -0400

    build(backend): include automatic migrations in start scripts

--- start.sh
+++ start.sh
@@ -8,5 +8,9 @@ npm run dev &
 # Start backend
 cd ../backend || exit
 source venv/bin/activate
+echo "Making migrations..."
+python manage.py makemigrations
+echo "Migrating..."
+python manage.py migrate
 echo "Starting backend..."
 python manage.py runserver
