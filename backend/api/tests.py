f1233e7dc668e785c45d56b7e5bde4770044ea45
Brandon Ly <git@brndn.ly>
2024-03-25 16:29:44 -0400

    test(backend): query api test

--- backend/api/tests.py
+++ backend/api/tests.py
@@ -1,3 +1,12 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
 
 # Create your tests here.
+
+class QueryDetailTests(APITestCase):
+    def test_get_query(self):
+        url = '/api/query' + '?limit=10&offset=0'
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.json()['result']['resource_id'], 'fac950c0-00d5-4ec1-a4d3-9cbebf98a305')
