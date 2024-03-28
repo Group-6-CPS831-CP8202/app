ddca4f6afe2ae63663b548846295c3f121ceb6e9
Brandon Ly <github@brndn.ly>
2024-03-28 19:22:10 -0400

    fix(backend): properly run migrations in .bat

--- start.bat
+++ start.bat
@@ -3,12 +3,12 @@ echo Starting frontend...
 cd frontend
 start cmd /k npm run dev
 
-cd backend
+cd ../backend
 call venv\Scripts\activate.bat
 echo Making migrations...
-start cmd /k python manage.py makemigrations
+python manage.py makemigrations
 echo Applying migrations...
-start cmd /k python manage.py migrate
+python manage.py migrate
 echo Starting backend...
 start cmd /k python manage.py runserver
 
