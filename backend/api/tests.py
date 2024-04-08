3aaff53617446d522bc1c3a762193ae4f0fd7aa7
Maisha2614 <mlabiba60@gmail.com>
2024-04-08 15:59:42 -0400

    Update tests.py

--- backend/api/tests.py
+++ backend/api/tests.py
@@ -22,3 +22,41 @@ class RegisterEndPointTests(APITestCase):
 
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(response.data, {'message': 'success'})
+
+#unit test
+from django.test import TestCase
+from myapp.serializers import MyModelSerializer
+
+class MyModelSerializerTest(TestCase):
+    def test_serializer_valid_data(self):
+        data = {'field1': 'value1', 'field2': 'value2'}
+        serializer = MyModelSerializer(data=data)
+        self.assertTrue(serializer.is_valid())
+
+
+#integration test
+from myapp.models import MyModel
+
+class MyModelAPITest(APITestCase):
+    def setUp(self):
+        MyModel.objects.create(user='value1', name='value2')
+
+    def test_get_my_model_list(self):
+        url = '/api/mymodel/'
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        #authentication test
+from django.contrib.auth.models import User
+from myapp.models import MyModel
+
+class MyModelAPITest(APITestCase):
+    def setUp(self):
+        user = User.objects.create_user(username='testuser', password='testpassword')
+        self.client.force_authenticate(user=user)
+
+    def test_authenticated_user_can_create_model(self):
+        url = '/api/mymodel/'
+        data = {'field1': 'value1', 'field2': 'value2'}
+        response = self.client.post(url, data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
